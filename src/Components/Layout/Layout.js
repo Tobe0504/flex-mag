c93ec2cfeb433bb9544e6fae68b2477a1ed17354
unknown <ezimorahtobenna@gmail.com>
2023-01-11 20:47:17 +0100

    Tried to commit

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Footer from "../../Containers/Footer/Footer";
 import Header from "../../Containers/Header/Header";
+import { AppContext } from "../../Context/AppContext";
 import classes from "./Layout.module.css";
 
 const Layout = (props) => {
+  // Context
+  const { fetchCompetition } = useContext(AppContext);
+
+  // Effect
+  useEffect(() => {
+    fetchCompetition();
+  });
   return (
     <div className={classes.container}>
       <div className={classes.header}>
--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { createContext, useState } from "react";
+import XMLParser from "react-xml-parser";
 
 export const AppContext = createContext();
 
@@ -7,9 +9,37 @@ const AppContextProvider = (props) => {
   const [displaySearch, setDisplaySearch] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
 
+  const fetchCompetition = () => {
+    axios
+      .get(
+        "/competition/news/7zWYv38S/300",
+        // { mode: "no-cors" },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            // "Access-Control-Allow-Origin": "*",
+            // "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          },
+        }
+      )
+      .then((res) => {
+        const jsonDataFromXml = new XMLParser().parseFromString(res.data);
+        console.log(jsonDataFromXml);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <AppContext.Provider
-      value={{ displaySearch, setDisplaySearch, searchQuery, setSearchQuery }}
+      value={{
+        displaySearch,
+        setDisplaySearch,
+        searchQuery,
+        setSearchQuery,
+        fetchCompetition,
+      }}
     >
       {props.children}
     </AppContext.Provider>
